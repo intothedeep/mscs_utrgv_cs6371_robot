058b83b3ca06540e09f21eb29892bdb7a5ef98ca
Taek Lim <tio.taek.lim@gmail.com>
2026-09-22 11:25:27 -0500

    fix: formatter

--- hw2/submission/Lim_20701313_hw2.py
+++ hw2/submission/Lim_20701313_hw2.py
@@ -1,37 +1,35 @@
-import numpy as np
 import math as m
-  
+
+import numpy as np
+
+
 # rotation matrix on x axis
 def Rx(theta):
-  return np.matrix([[ 1, 0           , 0           ],
-                   [ 0, m.cos(theta),-m.sin(theta)],
-                   [ 0, m.sin(theta), m.cos(theta)]])
-  
+    return np.matrix([[1, 0, 0], [0, m.cos(theta), -m.sin(theta)], [0, m.sin(theta), m.cos(theta)]])
+
+
 # rotation matrix on y axis
 def Ry(theta):
-  return np.matrix([[ m.cos(theta), 0, m.sin(theta)],
-                   [ 0           , 1, 0           ],
-                   [-m.sin(theta), 0, m.cos(theta)]])
+    return np.matrix([[m.cos(theta), 0, m.sin(theta)], [0, 1, 0], [-m.sin(theta), 0, m.cos(theta)]])
+
 
 # rotation matrix on z axis
 def Rz(theta):
-  return np.matrix([[ m.cos(theta),-m.sin(theta), 0],
-                   [ m.sin(theta), m.cos(theta), 0],
-                   [ 0           , 0           , 1]])
+    return np.matrix([[m.cos(theta), -m.sin(theta), 0], [m.sin(theta), m.cos(theta), 0], [0, 0, 1]])
 
 
 # please give the values of the three angles
-phi = m.pi/2
-theta = m.pi/4
-psi = m.pi/2
+phi = m.pi / 2
+theta = m.pi / 4
+psi = m.pi / 2
 print("phi =", phi)
 print("theta  =", theta)
 print("psi =", psi)
-  
-  
+
+
 # given the vector v1 =(1,2,3), calculate the vector v2 after the XYZ rotation.
 # X first, then Y, then Z: the matrix applied last stands leftmost.
-v1 = np.array([[1],[2],[3]])
+v1 = np.array([[1], [2], [3]])
 v2 = Rz(psi) * Ry(theta) * Rx(phi) * v1
 print("The vector v2 is", np.round(v2, decimals=2))
 
